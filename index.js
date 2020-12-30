b67179921bed043e6bd39c9cd27258036e20934e
PongPong3x <xfnyk113s@sina.com>
2020-12-31 02:09:00 +0800

    upload files

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const api = require('./api');
+const express = require('express');      
+const application= express();
+const port =4002;      
+
+application. listen(port, () => console.log('The application is listening to '+port))
+application.get('/add', (request, resonse) =>{
+    resonse.send('The add request resived');
+});
+
+application.get('/add2/:n/:m', (request, resonse) =>{
+    let n = Number(request.params.n);
+    let m = Number(request.params.m);
+    let sum = api.add(n,m);
+    resonse.send(`${n} + ${m} = ${sum}`);
+});
