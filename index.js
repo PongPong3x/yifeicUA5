6ca1fc5ed064809579f01b647cb26f321904bd51
PongPong3x <xfnyk113s@sina.com>
2021-01-03 17:21:15 +0800

    add change

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ application.get('/add', (request, resonse) =>{
     resonse.send('The add request resived');
 });
 
+
+
+
+
 application.get('/add2/:n/:m', (request, resonse) =>{
     let n = Number(request.params.n);
     let m = Number(request.params.m);
