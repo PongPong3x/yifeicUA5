6524a1ff351ed13b6cd2bcd9c00c9d14ee1b986a
PongPong3x <xfnyk113s@sina.com>
2021-01-11 07:27:32 +0800

    update3

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const application= express();
 const port =4002;      
 
 application.get('/add', (request, resonse) =>{
-    resonse.send('The add request resived');
+    response.send('The add request resived');
 });
 
 application.get('/add2/:n/:m', (request, resonse) =>{
     let n = Number(request.params.n);
     let m = Number(request.params.m);
     let sum = api.add(n,m);
-    resonse.send(`${n} + ${m} = ${sum}`);
+    response.send(`${n} + ${m} = ${sum}`);
 });
 application. listen(port, () => console.log('The application is listening to '+port))
