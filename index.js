96d1185dfc7d076c883ba465dec8b80d3aacce42
PongPong3x <xfnyk113s@sina.com>
2021-03-28 08:24:13 +0800

    change header 4

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 application.use(express.json())
 application.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
  })
 
