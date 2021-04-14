b1504a91add037ee7fe34e51cd59abae2675a9a1
PongPong3x <xfnyk113s@sina.com>
2021-04-14 23:23:31 +0800

    update header 8

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const port = process.env.PORT || 5000;
 
 application.use(express.json());
 application.use(cors());
-/*
+
 application.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     //res.header("Cache-Control","no-cache");
     next();
  })
-*/
+
 application.get('/add', (request, response) =>{
     response.send('The add request resived');
 });
