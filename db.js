8ad9ebbd3cba4b7825e0c125421d08a4f03a89ef
PongPong3x <xfnyk113s@sina.com>
2021-06-17 09:42:28 +0800

    update score

--- db.js
+++ db.js
@@ -67,14 +67,16 @@ let getFlowers= () =>{
 
 let addScore = (quizTaker,quizId, score) => {
     //scores.push({quizTaker,quizId,score});
-    return pool.query('insert into imagequiz.score(customer_id,quiz_id,score) values ($1,$2,$3)',
-    [quizTaker,quizId,score]);
+    return pool.query('select customer_id from imagequiz.customer where customer_id = $1',[quiztaker])
+    .then(x => pool.query(pool.query('insert into imagequiz.score(customer_id,quiz_id,score) values ($1,$2,$3)',[x.rows[0].customer_id,quizId,score])));
+    //return pool.query('insert into imagequiz.score(customer_id,quiz_id,score) values ($1,$2,$3)',
+    //[quizTaker,quizId,score]);
 }
 
 
 let checkScore = (quiztaker,quizid) => {
-    return pool.query('select score from imagequiz.score where (customer_id = $1 and quiz_id = $2)',
-    [quiztaker,quizId]);
+    return pool.query('select customer_id from imagequiz.customer where customer_id = $1',[quiztaker])
+    .then(x => pool.query('select score from imagequiz.score where (customer_id = $1 and quiz_id = $2)',[x.rows[0].customer_id,quizId]));
 }
 
 
