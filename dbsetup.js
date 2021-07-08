336bf36cfd423db632a0cc903eabdc8930770101
PongPong3x <xfnyk113s@sina.com>
2021-07-08 09:47:31 +0800

    update score

--- dbsetup.js
+++ dbsetup.js
@@ -1,7 +1,8 @@
+var {flowers} = require('./flowers');
 const { Pool } = require('pg');
 const fs = require('fs');
 const { quizzes } = require('./data');
-var {flowers} = require('./flowers');
+
 require('dotenv').config();
 let create_db_structure_sql = fs.readFileSync('db.sql').toString();
 const connectionString = `postgres://liknsyvyppedoj:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASEPORT}/${process.env.DATABASE}`;
@@ -10,11 +11,30 @@ const connection={
     ssl: {rejectUnauthorized: false}
 }
 const pool = new Pool(connection);
+let getInsertFlowersSql = () => {
+    let sql = "";
+    sql = `insert into imagequiz.flower(name, picture) values `;
+    let values = [];
+    for (flower of flowers) {
+      values.push(`('${flower.name}', '${flower.picture}')`);
+    }
+    sql += values.join(", ");
+    return sql;
+  };
+
+
 let getInsertQuizzesSql = (categoryId) => {
     let sql = '';
+    for (var i = 0; i < quizzes.length; i++) {
+        console.log(quizzes[i]);
+        sql += getInsertQuizSql(categoryId, quizzes[i])
+    }
+    /*
     for(quiz of quizzes) {
+        console.log(quiz);
        sql += getInsertQuizSql(categoryId, quiz)
     }
+    */
     return sql;
 }
 let getInsertQuizSql = (categoryId, quiz) => {
@@ -30,7 +50,7 @@ let getInsertQuizSql = (categoryId, quiz) => {
     sql += values.join(', ');
     sql += ' returning id )';
     sql += ` insert into imagequiz.quiz_question select (select id from quizid${quiz.id}), b.id from questionsid${quiz.id} b;`;
-    console.log(sql);
+    //console.log(sql);
     return sql;
 }
 pool.query(create_db_structure_sql)
@@ -42,5 +62,8 @@ pool.query(create_db_structure_sql)
     .then(x => pool.query(getInsertQuizzesSql(x.rows[0].id)))
     .then(x => console.log('The quizzes were inserted into the database.'))
     .catch(e => console.log(e)) 
-    .then(() => {for (var i = 0; i < flowers.length; i++) {pool.query('insert into imagequiz.flower(name,picture) values ($1,$2)',[flowers[i].name,flowers[i].picture]);}})
+    //.then(() => {for (var i = 0; i < flowers.length; i++) {pool.query('insert into imagequiz.flower(name,picture) values ($1,$2)',[flowers[i].name,flowers[i].picture]);}})
+    .then((x) => pool.query(getInsertFlowersSql()))
+    .then((x) => console.log("Flowers were succesfully added."))
+    .catch((e) => console.log(e));
     
